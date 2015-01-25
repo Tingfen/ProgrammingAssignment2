fb5f95c6cd41afce448209c9830017893104df10
Yan <yant@nimhd01629525.local>
2015-01-25 18:13:51 -0500

    assign2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script is for creating a function 'makeCacheMatrix' to make a matrix x
+## Create a funtion 'cacheSolve' to catch the running time of the inverse matric x
 
-## Write a short comment describing this function
+## Write a makeCacheMatrix function to make a matrix x
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  v<-null
+  set<-function(y) {
+    v<<-null
+  }
+  get<-function() x
+  setinvert<-function(solve) v <<- solve
+  getinvert<- function() v
+  list(set=set, get = get,
+       setinvert = setinvert,
+       getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
+## Write a function cacheSolve to catch the running time of inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        v<-x$getinvert()
+        if(!is.null(v)) {
+          message("getting cached data")
+          return(v)
+        }
+        data<-x$get()
+        v<-solve(data, ...)
+        x$setmean(v)
+        v
 }
